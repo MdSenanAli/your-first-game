38cf73df16a0866ef1fb5f41e81a2ff741ba353e
MohammadSenanAli <mdsenan@gmail.com>
2025-02-15 21:04:56 +0530

    video_2 completed

---
+++ video_2/completed_project/player/pirate.gd
@@ -0,0 +1,45 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	handle_direction(direction)
+	handle_animation()
+	move_and_slide()
+
+func handle_animation() -> void:
+	if not is_on_floor():
+		if velocity.y > 0:
+			$AnimatedSprite2D.play("fall")
+		else:
+			$AnimatedSprite2D.play("jump")
+	else:
+		if velocity.x:
+			$AnimatedSprite2D.play("run")
+		else:
+			$AnimatedSprite2D.play("idle")
+		
+func handle_direction(direction: float) -> void:
+	if direction > 0:
+		$AnimatedSprite2D.flip_h = false
+	elif direction < 0:
+		$AnimatedSprite2D.flip_h = true
